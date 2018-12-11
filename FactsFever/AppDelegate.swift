706831f6b83164a7885b051509eed32447b7ae85
shree bhagwat <shreebhagwat94@gmail.com>
2018-12-11 19:00:18 +0530

    badge counter not working

--- FactsFever/AppDelegate.swift
+++ FactsFever/AppDelegate.swift
@@ -31,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
         
         
         func userDidLogin(userId: String){
-            print("onesignal started.................")
             self.startOneSignal()
         }
     
@@ -39,32 +38,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
             let userId = note.userInfo!["userId"] as? String
             UserDefaults.standard.set(userId, forKey: "userId")
             UserDefaults.standard.synchronize()
-            print("user logged in ..............................................\(userId)")
             userDidLogin(userId: userId!)
         }
     
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound], completionHandler: { (granted, error) in
             })
-            DispatchQueue.main.async {
+            
                application.registerForRemoteNotifications()
-            }
+            
             
         } else {
             let types: UIUserNotificationType = [.alert, .badge, .sound]
             let settings = UIUserNotificationSettings(types: types, categories: nil)
             application.registerUserNotificationSettings(settings)
-            application.registerForRemoteNotifications()
+            
         }
         
-//        let oneSignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
-//        OneSignal.initWithLaunchOptions(launchOptions,
-//                                        appId: "c9b03431-8ca8-470c-ac7f-2b9c387e6d15",
-//                                        handleNotificationAction: nil,
-//                                        settings: oneSignalInitSettings)
-//        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
+        let oneSignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: "c9b03431-8ca8-470c-ac7f-2b9c387e6d15",
+                                        handleNotificationAction: nil,
+                                        settings: oneSignalInitSettings)
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
         
-          OneSignal.initWithLaunchOptions(launchOptions, appId: "c9b03431-8ca8-470c-ac7f-2b9c387e6d15", handleNotificationReceived: nil, handleNotificationAction: nil, settings: [kOSSettingsKeyInAppAlerts : false])
+//          OneSignal.initWithLaunchOptions(launchOptions, appId: "c9b03431-8ca8-470c-ac7f-2b9c387e6d15", handleNotificationReceived: nil, handleNotificationAction: nil, settings: [kOSSettingsKeyInAppAlerts : false])
         
         OneSignal.promptForPushNotifications(userResponse: { accepted in
             print("User accepted notifications: \(accepted)")
@@ -86,45 +84,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+      
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+     
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  
         FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+       
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
-        //        self.push.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-        
-        Auth.auth().setAPNSToken(deviceToken, type:AuthAPNSTokenType.prod)
-    }
-    
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        print("rem not with fetch")
-        
-        let firebaseAuth = Auth.auth()
-        if (firebaseAuth.canHandleNotification(userInfo)){
-            return
-        } else {
 
-        }
-        
-    }
 
     // MARK: OneSignal
     
